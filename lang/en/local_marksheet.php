70de41accea4f3a56eb49bc3deb9a5512f2c766b
Prosanto7 <prosantodeb7@gmail.com>
2023-07-10 13:23:41 +0600

    Added: language, local library and manage file

---
+++ lang/en/local_marksheet.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information
+ *
+ * @package    local_marksheet
+ * @copyright  2023, Prosanto Deb <prosanto.deb@brainstation-23.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Mark Sheet';
+$string['id'] = 'ID';
+$string['team1'] = 'Name of Team 1: ';
+$string['goal1'] = 'Goals scored by Team 1: ';
+$string['team2'] = 'Name of Team 2: ';
+$string['goal2'] = 'Goals scored by Team 2: ';
+$string['createoredit'] = 'Create or Edit a Entry';
+$string['updatethanks'] = 'Thanks for updating a record';
+$string['insertthanks'] = 'Thanks for inserting a record';
+$string['deletemessage'] = "The record was deleted successfully!";
+$string['managepagetitle'] = "Manage student marks";
+$string['deletescore'] = "Delete Scores";
+$string['deletetitle'] = "Confirm Delete";
+$string['modalmessage'] = "Do you really want to delete this amazing score?";
+$string['delete'] = "Delete";
---
+++ locallib.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information
+ *
+ * @package    local_marksheet
+ * @copyright  2023, Prosanto Deb <prosanto.deb@brainstation-23.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ function local_marksheet_display_marks() {
+    global $DB, $OUTPUT;
+    $marks = $DB->get_records('local_marksheet');
+
+    // Data to be passed in the manage template.
+    $templatecontext = (object) [
+        'texttodisplay' => array_values($marks),
+        'editurl' => new moodle_url('/local/marksheet/edit.php'),
+    ];
+
+    echo $OUTPUT->render_from_template('local_marksheet/manage', $templatecontext);
+}
